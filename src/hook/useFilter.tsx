eb3c6fce12aeee03cad9b4924603e83a7c1a04c0
Sawin3048 <94995320+Sawin3048@users.noreply.github.com>
2024-04-04 22:51:54 +0000

    change default filter selected to "all"

--- src/hook/useFilter.tsx
+++ src/hook/useFilter.tsx
@@ -3,7 +3,7 @@ import { FilterState, IFilterState } from '../const'
 import { IsetFilter } from '../types'
 
 export const useFilter = () => {
-  const [filter, setStateFilter] = useState<IFilterState>(FilterState.active)
+  const [filter, setStateFilter] = useState<IFilterState>(FilterState.all)
   const setFilter:IsetFilter = (filter)=> setStateFilter(filter)
   return {filter, setFilter}
 }
