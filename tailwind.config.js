46e306e87baa892644bd192ae7e9771d96d586c7
Samuel3048 <samuelalegre022@gmail.com>
2024-04-21 19:23:22 -0400

    install clsx and config with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  "tailwindCSS.experimental.classRegex": [
+    ["clsx\\(([^)]*)\\)", "(?:'|\"|`)([^']*)(?:'|\"|`)"]
+  ],
   theme: {
     extend: {}
   },
