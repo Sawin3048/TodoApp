0539452f3ee0e547c89b33d36279c44784b90f74
Samuel3048 <samuelalegre022@gmail.com>
2024-04-20 20:20:53 -0400

    try make offline page

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/index.js',
+        assetFileNames: 'assets/index.css',
+        chunkFileNames: "assets/chunk.js",
+        // manualChunks: undefined,
+      }
+    }
+  }
 })
