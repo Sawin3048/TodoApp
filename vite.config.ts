cf686815fa21cd48cec049982107578f238f1224
Samuel3048 <samuelalegre022@gmail.com>
2024-04-20 20:49:55 -0400

    Revert "try make offline page"
    This reverts commit 0539452f3ee0e547c89b33d36279c44784b90f74.

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/index.js',
-        assetFileNames: 'assets/index.css',
-        chunkFileNames: "assets/chunk.js",
-        // manualChunks: undefined,
-      }
-    }
-  }
+  plugins: [react()]
 })
